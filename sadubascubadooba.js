d92d0bd304eec71275ca068001f4e481f1b0be91
Dotdotspacespacecoloncolonunderscore <114034573+Dotdotspacespacecoloncolonunderscore@users.noreply.github.com>
2022-10-04 16:55:46 -0700

    llll

---
+++ sadubascubadooba.js
@@ -0,0 +1,11 @@
+
+function kadooba(){
+    document.getElementById("reasons-to-be-friends-with-me").innerHTML="1:i share my cookies with you:)<br>\n2:im not a kidnapper anymore<br>\n3:my mom makes good meatloaf"
+
+
+
+
+
+
+
+}
