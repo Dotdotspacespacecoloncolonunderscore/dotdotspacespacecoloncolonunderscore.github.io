7efe7500328df7457bc6fa54b56d317a64ad8ce8
Dotdotspacespacecoloncolonunderscore <114034573+Dotdotspacespacecoloncolonunderscore@users.noreply.github.com>
2022-10-04 17:31:42 -0700

    uuy

--- sadubascubadooba.js
+++ sadubascubadooba.js
@@ -1,6 +1,38 @@
 
 function kadooba(){
-    document.getElementById("reasons-to-be-friends-with-me").innerHTML="1:i share my cookies with you:)<br>\n2:im not a kidnapper anymore<br>\n3:my mom makes good meatloaf"
+   var namboo = ["i share my cookies with you",
+"im not a kidnapper",
+'anymore',
+"my mom makes good meaatloaf",
+"i like good music",
+"creator of mombe de monke",
+'good green screen editor',
+'i play de geetar',
+'#juan gamer',
+'i know that jaun plus jaun equals horse',
+'i make good eggs',
+'i know what mems are and how to use them',
+'i know how to pronunce saskackspank',
+'i know how to cure LIGMA',
+'i can get ggrapes from a lemonade stand',
+'i watch sesame street on wekends',
+'i can make a good wesit',
+'i know magic here is spell',
+'siggity soggity turn her in to a froggity',
+'i get baba greels',
+'i can use the potty',
+'sometimes',] 
+
+var text = ""
+for(let i=0;i<namboo.length;i++){
+
+text+=i
+text+=":"
+text+=namboo[i]
+text+="<br>"
+
+}
+    document.getElementById("reasons-to-be-friends-with-me").innerHTML=text
 
 
 
