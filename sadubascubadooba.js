04e7ccaac664c27197010fec82a0b9d7a5978650
Dotdotspacespacecoloncolonunderscore <114034573+Dotdotspacespacecoloncolonunderscore@users.noreply.github.com>
2022-10-04 17:44:14 -0700

    ffhhgf

--- sadubascubadooba.js
+++ sadubascubadooba.js
@@ -1,8 +1,7 @@
 
 function kadooba(){
    var namboo = ["i share my cookies with you",
-"im not a kidnapper",
-'anymore',
+"im not a kidnapper<br>anymore",
 "my mom makes good meaatloaf",
 "i like good music",
 "creator of mombe de monke",
@@ -16,17 +15,15 @@ function kadooba(){
 'i know how to cure LIGMA',
 'i can get ggrapes from a lemonade stand',
 'i watch sesame street on wekends',
-'i can make a good wesit',
-'i know magic here is spell',
-'siggity soggity turn her in to a froggity',
+'i can make a good websit',
+'i know magic here is spell<br>siggity soggity turn her in to a froggity',
 'i get baba greels',
-'i can use the potty',
-'sometimes',] 
+'i can use the potty<br>sometimes'] 
 
 var text = ""
 for(let i=0;i<namboo.length;i++){
 
-text+=i
+text+=i+1
 text+=":"
 text+=namboo[i]
 text+="<br>"
